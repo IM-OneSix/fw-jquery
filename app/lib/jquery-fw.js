d6d7b6bd7a5c0dda837194181589a7c623abf763
gitkhs <parts1123@naver.com>
2016-11-06 23:50:04 +0900

    수정

--- app/lib/jquery-fw.js
+++ app/lib/jquery-fw.js
@@ -7,84 +7,80 @@
 			return b=='I'?(m?['I',v,m]:['I',7,7]):[b,99,99];
 		}()
 	};
-	var _view={};
+	var _controller={};
 	var _service={};
 
-	_w.main= function(fn) {
+	_w.main= function(f) {
 		$(document).ready(function() {
+			_.each(['controller', 'service'],function(v){delete _w[v]});
 			if($('[data-bind-view=main]').length < 1) {
 				alert('main이 존재하지 않습니다.');
 				return;
 			}
-			_w.view('main', fn);
-			_.each(['controller', 'service'],function(v){delete _w[v]});
 
-			loadView('main');
+			$('[data-bind-view]').on('click', function(e) {
+				return (function(c,t){
+					var $vo=_controller[c]['vo'];
+					t && eval('$vo.'+t);
+					return !t;
+				})($(e.currentTarget).data('bindView'), $(e.target).data('bindClick'));
+			}).on('change', function(e) {
+				(function(c,t){
+					var $vo=_controller[c]['vo'];
+					t && eval('$vo.'+t);
+				})($(e.currentTarget).data('bindView'), $(e.target).data('bindChange'));
+			});
+
+			addController('main', f);
+			loadController('main');
 		});
 	};
-	_w.view= function(name, fn) {
-		_view[name]={
-			controller: fn,
-			model: {$name:name}
+	_w.loadView= function(n) {
+		_.isArray(n) && _.each(n, function(v){loadController(n);});
+	};
+
+	_w.controller= function(n,f) {
+		addController(n,f);
+	};
+	function loadController(n) {
+		var func=_controller[n]['func'], vo=_controller[n]['vo'];
+		// bindIf
+		// bindEach
+		func(vo), updateData(n);
+		// _controller[n]['func'](_controller[n]['vo'], svc()), updateData(n);
+	}
+	function addController(n, f) {
+		if(_controller[n]) {
+			alert(n+': 컨트롤러가 중복됩니다.');
+			return;
+		}
+		_controller[n] = {
+			func: f,
+			vo: {},
+			bindIf: {},
+			bindEach: {}
 		};
-		$log.out('view', _view);
-	};
-	_w.service= function(name, fn) {
-		_service[name]=fn;
-		$log.out('_service', name, _service);
-	};
-
-	function svc() {
-		return {
-			get: function(name) {return _service[name](svc())}
-		}
 	}
-	function loadView(name) {
-		// $(element).off('keydown.SIE#Pw keyup.SIE#Pw').on('keydown.SIE#Pw keyup.SIE#Pw',function(e){
-		// 	var v=valueAccessor();
-		// 	var keyCode = e.keyCode || e.which;
-		// 	if(typeof v==='function'){
-		// 		v($(e.target).val());
-		// 	}
-		// });
-
-		$($('[data-bind-view='+name+']')[0]).click(function(e) {
-			$(e.target).data('bindClick') && eval('_view.'+name+'.model.'+$(e.target).data('bindClick'));
+	function updateData(n) {
+		var $view = $($('[data-bind-view='+n+']')[0]);
+		// text
+		_.each($view.find('[data-bind-text]'), function(v) {
+			(function($vo,t){
+				t.text(eval('$vo.'+t.data('bindText')))
+			})(_controller[n]['vo'], $(v));
 		});
-		_view[name]['controller'](_view[name]['model'], svc());
+		// html
+		_.each($view.find('[data-bind-html]'), function(v) {
+			(function($vo,t){
+				t.html(eval('$vo.'+t.data('bindHtml')))
+			})(_controller[n]['vo'], $(v));
+		});
 	}
 
-	w._w=_w;
-	w.$log={
-		out: function() {
-			w.console || {log:function(){}};
-			var a = _.toArray(arguments);
-			(function(ie){ie&&console.log(JSON.stringify(a)),!ie&&console.log.apply(w, a)})('I'==_w.browser[0])
-		}
+	w.root=_w;
+	w.$log=function() {
+		w.console || {log:function(){}};
+		var a = _.toArray(arguments);
+		(function(ie){ie&&console.log(JSON.stringify(a)),!ie&&console.log.apply(w, a)})('I'==_w.browser[0])
 	};
-
-
-_w.service('io', function($svc) {
-	return {
-		push: function($vo) {
-			var vi= $('[data-bind-view='+$vo.$name+']')[0];
-			// if
-			// visible
-			// each
-
-			// text
-			_.each($(vi).find('[data-bind-text]'), function(v) {
-				$(v).text(eval('$vo.'+$(v).data('bindText')));
-			});
-			// html
-			_.each($(vi).find('[data-bind-html]'), function(v) {
-				$(v).html(eval('$vo.'+$(v).data('bindHtml')));
-			});
-			// value
-		}
-	}
-});
-_w.service('lpopup', function($svc) {
-});
-
 })(window);
