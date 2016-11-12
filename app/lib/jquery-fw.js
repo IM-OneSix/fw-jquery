9e8c6f10a5239ac89f570c78485866c8cf6e9c42
gitkhs <parts1123@naver.com>
2016-11-13 01:47:31 +0900

    mod

--- app/lib/jquery-fw.js
+++ app/lib/jquery-fw.js
@@ -14,8 +14,9 @@
 		root.j$ = j$;
 	}
 
-	_.templateSettings = {evaluate:/\{\{(.+?)\}\}/g,interpolate: /\{\{=(.+?)\}\}/g, escape: /\{\{-(.+?)\}\}/g};
+	_.templateSettings = {evaluate:/\{\{(.+?)\}\}/g,interpolate: /\{\{=(.+?)\}\}/g, escape: /\{\{-(.+?)\}\}/g};//, variable:'$data'
 	var controller={}, service={};
+	j$.debug=true,
 	j$.browser = function() {
 		var m=document.documentMode,a=navigator.userAgent,
 		b=_.find({'Chrome/':'C','Safari/':'S','Firefox/':'F','OPR':'O','Opera':'O','Trident/':'I','MSIE':'I'},function(x,y){return a.indexOf(y)>-1})||'N',
@@ -23,34 +24,12 @@
 		return b=='I'?(m?['I',v,m]:['I',7,7]):[b,99,99];
 	}();
 	j$.main = function(func){
-		delete j$.main;
-
 		$(document).ready(function() {
-			if($('[data-bind-view=main]').length < 1) {
-				error('main이 존재하지 않습니다.');return;
-			}
-
-			_.each(['controller', 'service'],function(v){delete j$[v]});
-			$('[data-bind-view]').on('click', function(e) {
-				return execute($(e.currentTarget).data('bindView'), $(e.target).data('bindClick'));
-			}).on('change', function(e) {
-				return execute($(e.currentTarget).data('bindView'), $(e.target).data('bindChange'));
-			});
-
+			_.each(['main','controller', 'service'],function(v){delete j$[v]});
 			addController('main', func);
 			loadController('main');
 		});
-		function execute(name,target) {
-			if(!target)return!0;
-			var e=$vo=controller[name]['vo'],
-			f=function(s){return s.length<2 ? null : [s[0].split('.'),s[1].split(')')[0].split(',')]}(target.split('('));
-			_.each(f[0],function(v){e[v]&&(e=e[v])}), _.isFunction(e)?e.apply($vo,f[1]):error('undefined function', '$vo.'+f[0]);
-			return!1;
-		}
 	};
-	// j$.loadView 화면 출력 후 컨트롤러 로드
-	// j$.closeView 화면 히든 또는 삭제 후 컨트롤러 삭제
-	// j$.moveView 호출한 컨트롤러 히든 또는 삭제, 화면 출력 후 컨트롤러 로드
 	j$.controller = addController;
 	j$.service = function(name, func) {
 		if(service[name]) {
@@ -59,36 +38,120 @@
 		service[name]=func;
 	};
 	function addController(name, func) {
-		if(controller[name]) {
-			error('컨트롤러 중복: ', name);return;
-		}
+		controller[name] && error('컨트롤러 중복 선언: '+name);
 		controller[name] = {
-			bind: func,
-			vo: {},
-			tIf: {},
-			tEach: {}
+			$bind: func,
+			$vo: {},
+			$if: {},
+			$each: {}
 		};
 	}
 	function loadController(name) {
+		$('[data-bind-view='+name+']').length < 1 && (delete controller[name],error('뷰 엘리먼트 미존재: '+name));
+		$('[data-bind-view='+name+']').length > 1 && (delete controller[name],error('뷰 엘리먼트 중복: '+name));
+
 		// element-if, each
 		// data-bind-link: 바인딩 참조된 객체 이름
 		// _.each($('[data-bind-view='+n+'] [data-bind-if]'), function(v,k) {
 		// 	_controller[n]['bindIf'][k]=$(v).html(), $(v).html('');
 		// });
-		// _.each($('[data-bind-view='+n+'] [data-bind-each]'), function(v,k) {
-		// 	_controller[n]['bindEach'][k]=$(v).html(), $(v).html('');
-		// });
-		var con = controller[name];
-		con['bind'](con['vo'], svc());//, updateData(n);
+
+		var $c=controller[name];
+		var $bind=$c['$bind'],$vo=$c['$vo'],$each=$c['$each'];
+
+		_.each($('[data-bind-view='+name+'] [data-bind-each]'), function(v,k) {
+			var h=$(v).html();
+			_.each($(v).find('[data-bind-click]'), function(a){
+				$(a).attr('data-bind-item', $(v).data('bindEach')+',{{=$index}}');
+			});
+			_.each($(v).find('[data-bind-change]'), function(a){
+				$(a).attr('data-bind-item', $(v).data('bindEach')+',{{=$index}}');
+			});
+			$each[$(v).data('bindEach')]=_.template($(v).html());
+		});
+		$bind($vo, svc()), wacth(name);
 	}
+	function wacth(name) {
+		var $c=controller[name];
 
+// attr
+// class
+		_.each($('[data-bind-view='+name+'] [data-bind-text]'), function(v) {
+			(function(a){
+				$(v).text(eval('$c.$vo.'+a))
+			})($(v).data('bindText'))
+		});
+		_.each($('[data-bind-view='+name+'] [data-bind-html]'), function(v) {
+			(function(a){
+				$(v).html(eval('$c.$vo.'+a))
+			})($(v).data('bindHtml'))
+		});
+		_.each($('[data-bind-view='+name+'] [data-bind-each]'), function(v) {
+			root.$index=0,root.$data={};
+			(function(a){
+				var tx='',
+				tp=eval('$c.$each[\''+a+'\']'),
+				dt=eval('$c.$vo.'+a);
 
+				_.each(dt, function(d,i){
+					root.$index=i,
+					root.$data=d,
+					tx+=tp(d);
+				}),
+				$(v).html(tx);
+			})($(v).data('bindEach'))
+			delete root.$index, delete root.$data;
+		});
+		_.each($('[data-bind-view='+name+'] [data-bind-visible]'), function(v,k) {
+			(function(a){
+				if(a=='true') return $(v).show();
+				var vl=(eval('$c.$vo.'+a)+'')!='false';
+				a.indexOf('==')>0 && (vl=eval('$c.$vo.'+a.split('==')[0])==a.split('==')[1].replace(/'/g,''));
+				vl ? $(v).show():$(v).hide();
+			})($(v).data('bindVisible'))
+		});
 
+		// 이벤트 바인딩
+		$('[data-bind-view='+name+'] [data-bind-click]').off('click').on('click', function(e){
+			return execute(name, $(e.currentTarget).data('bindClick'), $(e.currentTarget).data('bindItem'));
+		});
+		$('[data-bind-view='+name+'] [data-bind-change]').off('change').on('change', function(e){
+			return execute(name, $(e.currentTarget).data('bindChange'), $(e.currentTarget).data('bindItem'));
+		});
+		function execute(name,target,item) {
+			if(!target)return!0;
+
+			var $c=controller[name], func='', arg='';
+			var e=$vo=$c['$vo'];
+
+			// 함수부, 파라메터부 파싱
+			(function(s){
+				s.length<2 ? null : (func=s[0].split('.'), arg=s[1].split(')')[0].split(','))
+			})(target.split('('));
+
+			// 함수 선언여부 체크
+			_.each(func,function(v){e[v]&&(e=e[v])});
+			!_.isFunction(e) && error('undefined function: $vo.'+func.join('.'));
+
+			// 함수 호출시 넘겨줄 데이타 파싱
+			var i = item&&item.split(',');
+			_.isArray(i) && ($vo=eval('$c.$vo.'+i[0])[i[1]]);
+
+			e.apply($vo,arg),wacth(name);
+			return!1;
+		}
+	}
 	function svc() {
 		return {
 			get: function(name) {return service[name](svc())}
 		}
 	}
+
+	// 서비스 구현
+	// ==================================================
+	// j$.loadView 화면 출력 후 컨트롤러 로드
+	// j$.closeView 화면 히든 또는 삭제 후 컨트롤러 삭제
+	// j$.moveView 호출한 컨트롤러 히든 또는 삭제, 화면 출력 후 컨트롤러 로드
 	service.log=function(){
 		return {
 			group:function(b){'I'!=j$.browser[0] && b && console.group(b)},
@@ -97,10 +160,8 @@
 	};
 
 	root.$log=function() {
-		'I'==j$.browser[0]?console.log(JSON.stringify(_.toArray(arguments))):console.log.apply(root, _.toArray(arguments));
+		j$.debug && ('I'==j$.browser[0]?console.log(JSON.stringify(_.toArray(arguments))):console.log.apply(root, _.toArray(arguments)));
 	};
-	function error() {
-		'I'==j$.browser[0]?console.log('error', JSON.stringify(_.toArray(arguments))):console.error.apply(root, _.toArray(arguments));
-	}
+	function error(msg) {throw new Error(msg)}
 })(window);
 
