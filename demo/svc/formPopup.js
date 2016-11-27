1b56c45cbbf17682ee41d9345a518dcd64282083
gitkhs <parts1123@naver.com>
2016-11-27 12:33:10 +0900

    khs

--- demo/svc/formPopup.js
+++ demo/svc/formPopup.js
@@ -1,6 +1,6 @@
 (function(){
 $svc.popup('lpFormPopup',function(param, $close){
-	var vo=$svc.bind({name:'lpFormPopup', url:'//fw-jquery/demo/svc/formPopup.html'});
+	var vo=$svc.bind({name:'lpFormPopup', url:'//im-onesix.github.io/fw-jquery/demo/svc/formPopup.html'});
 	var on=vo.event();
 	vo.render();
 
