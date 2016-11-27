29e79f7527b778cb2aef03733a70beea41b4d322
gitkhs <parts1123@naver.com>
2016-11-27 12:50:29 +0900

    khs

--- demo/svc/form.js
+++ demo/svc/form.js
@@ -9,9 +9,19 @@ $svc.view('form',function(param){
 	vo.render();
 
 	on.clickPopup=function(){
-		$svc.get('popup').open({name:'lpFormPopup'}).then(function(rs){
-			$log(rs);
-		});
+		// $.ajax({
+		// 					type:'post',
+		// 					contentType:'text/html',
+		// 					url:uri(url,cache),
+		// 					success:d.resolve,
+		// 					error:function(){d.resolve(null)}
+		// });
+$.post("formPopup.html", function(data) {
+   $log('-', data);
+ });
+		// $svc.get('popup').open({name:'lpFormPopup'}).then(function(rs){
+		// 	$log(rs);
+		// });
 	};
 	on.clickView=function(){
 		var v=vo.pull({
